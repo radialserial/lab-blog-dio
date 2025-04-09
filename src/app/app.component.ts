4e09e386050039f6d7d323dced81dd07e559579d
Natanael Alves Fernandes <natanael.alves.fernandes@gmail.com>
2025-04-09 18:34:30 -0300

    feat: add title and menu bar to home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TitleBarComponent } from "./components/title-bar/title-bar.component";
+import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TitleBarComponent, MenuBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { TitleBarComponent } from "../../components/title-bar/title-bar.component";
+import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [TitleBarComponent, MenuBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
